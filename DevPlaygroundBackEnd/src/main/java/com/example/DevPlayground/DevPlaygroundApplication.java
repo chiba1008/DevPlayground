c8325d00ef73e4a74122fb50cc55dbf53201a246
chiba <c.keigo1008@gmail.com>
2025-09-21 13:30:44 +0900

    Initial commit

---
+++ DevPlaygroundBackEnd/src/main/java/com/example/DevPlayground/DevPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevPlayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevPlaygroundApplication.class, args);
+	}
+
+}
---
+++ DevPlaygroundBackEnd/src/test/java/com/example/DevPlayground/DevPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevPlayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
