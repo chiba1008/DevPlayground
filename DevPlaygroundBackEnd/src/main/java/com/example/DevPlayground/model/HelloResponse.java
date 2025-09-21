f820c0f729ec13df5a819f8f6a321b7ea934b544
chiba <c.keigo1008@gmail.com>
2025-09-21 14:13:51 +0900

    Refactor HelloResponse to use record for concise data representation

--- DevPlaygroundBackEnd/src/main/java/com/example/DevPlayground/model/HelloResponse.java
+++ DevPlaygroundBackEnd/src/main/java/com/example/DevPlayground/model/HelloResponse.java
@@ -1,11 +1,4 @@
 package com.example.DevPlayground.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class HelloResponse {
-    private final String message;
-    private final String status;
+public record HelloResponse(String message, String status) {
 }
